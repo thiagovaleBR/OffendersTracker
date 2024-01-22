d5f5617a50ab99e467b2dbdc98278fd49c5433c2
Thiago André Cardoso Silva <33556919+thiagoandrecardoso@users.noreply.github.com>
2024-01-22 17:14:30 -0300

    Add builder pattern and small clean code.

--- offenders/src/main/java/br/com/eletra/offenders/services/impl/AppointmentServiceImpl.java
+++ offenders/src/main/java/br/com/eletra/offenders/services/impl/AppointmentServiceImpl.java
@@ -2,10 +2,13 @@ package br.com.eletra.offenders.services.impl;
 
 import br.com.eletra.offenders.dtos.CreateAppointmentDto;
 import br.com.eletra.offenders.entities.AppointmentEntity;
+import br.com.eletra.offenders.entities.AreaEntity;
+import br.com.eletra.offenders.entities.LineEntity;
 import br.com.eletra.offenders.repositories.AppointmentRepository;
 import br.com.eletra.offenders.repositories.AreaRepository;
 import br.com.eletra.offenders.repositories.LineRepository;
 import br.com.eletra.offenders.services.AppointmentService;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -30,17 +33,33 @@ public class AppointmentServiceImpl implements AppointmentService {
     }
 
     public AppointmentEntity create(CreateAppointmentDto createAppointmentDto) {
-        var appointment = new AppointmentEntity();
-        var line = lineRepository.findById(createAppointmentDto.getLineId()).orElseThrow();
-        var offender = areaRepository.findById(createAppointmentDto.getOffenderId()).orElseThrow();
-        appointment.setDate(createAppointmentDto.getDate());
-        appointment.setLine(line);
-        appointment.setOffender(offender);
-        appointment.setStartTime(createAppointmentDto.getStartTime());
-        appointment.setEndTime(createAppointmentDto.getEndTime());
-        appointment.setDescription(createAppointmentDto.getDescription());
-        appointment.setTicketId(createAppointmentDto.getTicketId());
-        appointment.setAppointer(createAppointmentDto.getAppointer());
+
+        var line = getLineEntity(createAppointmentDto);
+        var offender = getAreaEntity(createAppointmentDto);
+
+        AppointmentEntity appointment = AppointmentEntity.builder()
+                .date(createAppointmentDto.getDate())
+                .line(line)
+                .offender(offender)
+                .startTime(createAppointmentDto.getStartTime())
+                .endTime(createAppointmentDto.getEndTime())
+                .description(createAppointmentDto.getDescription())
+                .ticketId(createAppointmentDto.getTicketId())
+                .appointer(createAppointmentDto.getAppointer())
+                .build();
+
         return appointmentRepository.save(appointment);
+    }
+
+    private AreaEntity getAreaEntity(CreateAppointmentDto createAppointmentDto) {
+        return areaRepository.findById(createAppointmentDto.getOffenderId())
+                .orElseThrow(() -> new EntityNotFoundException("Offender not found with ID: " +
+                        createAppointmentDto.getOffenderId()));
+    }
+
+    private LineEntity getLineEntity(CreateAppointmentDto createAppointmentDto) {
+        return lineRepository.findById(createAppointmentDto.getLineId())
+                .orElseThrow(() -> new EntityNotFoundException("Line not found with ID: " +
+                        createAppointmentDto.getLineId()));
     }
 }
