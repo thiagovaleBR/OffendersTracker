0d668539e4da9578aa5d4625fb931980856f6317
Thiago Vale <thiagovale17@gmail.com>
2024-04-25 23:48:06 -0300

    Appointment with mapstruct

--- offenders/src/main/java/br/com/eletra/offenders/mappers/AppointmentMapper.java
+++ offenders/src/main/java/br/com/eletra/offenders/mappers/AppointmentMapper.java
@@ -5,45 +5,24 @@ import br.com.eletra.offenders.dtos.appointment.CreateAppointmentDto;
 import br.com.eletra.offenders.entities.AppointmentEntity;
 import br.com.eletra.offenders.entities.AreaEntity;
 import br.com.eletra.offenders.entities.LineEntity;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Component;
+import org.mapstruct.Mapper;
 
-@Component
-@RequiredArgsConstructor
-public class AppointmentMapper {
+import java.util.List;
 
-    private final LineMapper lineMapper;
-    private final AreaMapper areaMapper;
+@Mapper(componentModel = "spring")
+public abstract class AppointmentMapper {
 
-    public AppointmentDto toAppointmentDto(AppointmentEntity appointmentEntity) {
-        AppointmentDto appointmentDto = new AppointmentDto();
-        appointmentDto.setId(appointmentEntity.getId());
-        appointmentDto.setDate(appointmentEntity.getDate());
-        appointmentDto.setLine(lineMapper.toIdAndNameDto(appointmentEntity.getLine()));
-        appointmentDto.setOffender(areaMapper.toIdAndNameDto(appointmentEntity.getOffender()));
-        appointmentDto.setStartTime(appointmentEntity.getStartTime());
-        appointmentDto.setEndTime(appointmentEntity.getEndTime());
-        appointmentDto.setDescription(appointmentEntity.getDescription());
-        appointmentDto.setTicketId(appointmentEntity.getTicketId());
-        appointmentDto.setAppointer(appointmentEntity.getAppointer());
-        return appointmentDto;
-    }
+    public abstract AppointmentDto toAppointmentDto(AppointmentEntity appointmentEntity);
+
+    public abstract List<AppointmentDto> toAppointmentDtoList(List<AppointmentEntity> appointmentEntityList);
+
 
     public AppointmentEntity createEntity(CreateAppointmentDto createAppointmentDto, LineEntity line, AreaEntity offender) {
         var appointment = new AppointmentEntity();
         return updateEntity(appointment, createAppointmentDto, line, offender);
     }
 
-    public AppointmentEntity updateEntity (AppointmentEntity appointment, CreateAppointmentDto createAppointmentDto, LineEntity line, AreaEntity offender) {
-        appointment.setDate(createAppointmentDto.getDate());
-        appointment.setLine(line);
-        appointment.setOffender(offender);
-        appointment.setStartTime(createAppointmentDto.getStartTime());
-        appointment.setEndTime(createAppointmentDto.getEndTime());
-        appointment.setDescription(createAppointmentDto.getDescription());
-        appointment.setTicketId(createAppointmentDto.getTicketId());
-        appointment.setAppointer(createAppointmentDto.getAppointer());
-        return appointment;
-    }
+    public abstract AppointmentEntity updateEntity(AppointmentEntity appointment, CreateAppointmentDto createAppointmentDto, LineEntity line, AreaEntity offender);
+
 
 }
